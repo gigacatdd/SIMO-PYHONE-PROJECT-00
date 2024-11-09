269204eb58358ae3b3d7ac8ee550b8a68321ef74
gigacatdd <git config --global user.email>
2024-11-09 14:32:31 +0100

    what

--- simo.py
+++ simo.py
@@ -1,8 +1,61 @@
 import turtle
+import time
+import random
 wn = turtle.Screen()
 wn.title('snake game🐍')
-wn.setup(width=500, height=700)
+wn.setup(width=600, height=600)
 wn.bgcolor("black")
-input(wn)
+wn.cv._rootwindow.resizable(False, False)
 
+
+head = turtle.Turtle()
+head.shape('square')
+head.color('white')
+head.goto(0, 0)
+head.speed(0)
+head.deriction = 'stop'
+
+
+food = turtle.Turtle()
+got = random.randint(1,600)
+colors = random.choice(['red', 'blue', 'green'])
+shapes = 'square'
+food.goto(got)
+food.penup()
+food.speed()
+
+
+pen = turtle.Turtle()
+pen.shape('square')
+pen.color('white')
+pen.penup()
+pen.speed()
+pen.goto(0, 250)
+pen.hideturtle()
+pen.write('score: 0 high score: 0', align='center', font=("Arial", 24, "bold"))
+def goup():
+    if head.direction != 'down':
+        head.direction = 'up'
+def godown():
+    if head.direction != 'up':
+        head.direction = 'down'
+def goleft():
+    if head.direction != 'right':
+        head.direction = 'left'
+def goright():
+    if head.direction != 'left':
+        head.direction = 'right'
+def ff():
+    if head == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+    if head == 'down':
+        y = head.ycor()
+        head.sety(y - 20)
+    if head == 'left':
+        x = head.xcor()
+        head.setx(x - 20)
+    if head == 'right':
+        x = head.xcor()
+        head.setx(x + 20)
 
---
+++ simo/simo.py
@@ -0,0 +1,7 @@
+import turtle
+import random
+wn = turtle.Screen()
+wn.title('snake game🐍')
+wn.setup(width=600, height=600)
+wn.bgcolor("black")
+wn.write('hello')
